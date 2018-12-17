5a5618018da2d9200ec45b27fac275f12d9366cc
Aloiso Gomes <32063861+aloisogomes@users.noreply.github.com>
2018-12-17 15:36:44 -0200

    Update a920.js

--- www/a920.js
+++ www/a920.js
@@ -1,1 +1,21 @@
+// Empty constructor
+function A920() {}
 
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+A920.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
+}
+
+// Installation constructor that binds ToastyPlugin to window
+A920.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.toastyPlugin = new A920();
+  return window.plugins.toastyPlugin;
+};
+cordova.addConstructor(A920.install);
